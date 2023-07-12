92f45b8265914062410327601c07de9f8fea5ecc
enricoxD <noreply@enricoe.de>
2023-07-12 22:05:52 +0200

    add base url cosnt

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = process.env.API_BASE_URL;
+export const BASE_URL = process.env.BASE_URL;
+
 export const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: API_BASE_URL,
     withCredentials: true,
     headers: {'Content-Type': 'application/json'}
 });
 
 export const apiFormData = axios.create({
-    baseURL: BASE_URL,
+    baseURL: API_BASE_URL,
     withCredentials: true,
     headers: {'Content-Type': 'multipart/form-data'}
 });
