25a2e5bc626a1942bf2d968ae3394dab3b08a7c8
enricoxD <noreply@enricoe.de>
2023-07-25 18:56:41 +0200

    organize imports

--- src/hooks/getCurrentUser.tsx
+++ src/hooks/getCurrentUser.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import {UserType} from "@/utils/baseTypes";
 import {api} from "@/utils/api";
 import {useStickyState} from "@/hooks/useStickyState";
