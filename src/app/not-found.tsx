b5a6359c8fa0941ffc136c791eff87d4b204b81e
enricoxD <noreply@enricoe.de>
2023-07-26 17:06:42 +0200

    add a really simple 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import {Button} from "@/components/Button";
+
+export default function NotFoundPage() {
+  return (
+    <main>
+      <section className={"hero-section section is-flex-column"}>
+        <h1 className={"title error-text"}>This page cannot be found...</h1>
+        <h2 className={"subtitle"} style={{marginBottom: "15rem"}}>Error 404</h2>
+        <Button href={"/"} layout={"gradient"}>
+          <p>Go back</p>
+        </Button>
+      </section>
+    </main>
+  )
+}
